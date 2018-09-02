152895160651a642096a8673a8d299bc70c0d3df
Hidenori Tanaka <hidenori091985@yahoo.co.jp>
2018-09-02 16:09:27 +0900

    Initial Commit

---
+++ book_designPattern_swift_githubTests/book_designPattern_swift_githubTests.swift
@@ -0,0 +1,36 @@
+//
+//  book_designPattern_swift_githubTests.swift
+//  book_designPattern_swift_githubTests
+//
+//  Created by 田中 秀典 on 2018/09/02.
+//  Copyright © 2018年 田中 秀典. All rights reserved.
+//
+
+import XCTest
+@testable import book_designPattern_swift_github
+
+class book_designPattern_swift_githubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
